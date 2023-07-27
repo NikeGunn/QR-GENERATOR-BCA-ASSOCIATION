edc597cbd32ee5c5d8f53fc6b0fbceb13d86cc2e
NIkhil <knewboy.nykhil@gmail.com>
2023-07-27 21:41:36 +0545

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,80 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import axios from 'axios';
+import QRCode from 'qrcode.react';
 import './App.css';
 
 function App() {
+  const { register, handleSubmit, reset } = useForm();
+  const [id, setID] = useState('');
+
+  const onSubmit = async (data) => {
+    try {
+      const response = await axios.post('https://mmamc-api-college.onrender.com/api/students', {
+        ...data
+      });
+      console.log(response.data);
+      setID(response.data._id);
+      reset();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleDownload = () => {
+    const canvas = document.querySelector("canvas");
+    const qrCodeURL = canvas.toDataURL("image/png");
+    const downloadLink = document.createElement("a");
+    downloadLink.href = qrCodeURL;
+    downloadLink.download = "mmamcbca_qrcode.png";
+    downloadLink.click();
+  };
+
+  const handleInputChange = (event) => {
+    event.target.value = event.target.value.toUpperCase();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <nav className="navbar">
+        <h1 className="navbar-title">MMAMC BCA ASSOCIATION </h1>
+        <h2 className="navbar-subtitle">ID CARD GENERATOR </h2>
+      </nav>
+      <form className="form" onSubmit={handleSubmit(onSubmit)}>
+        <input
+          className="input"
+          type="text"
+          placeholder="Name"
+          {...register('name', { required: true })}
+          onInput={handleInputChange} // Convert input to uppercase
+        />
+        <input
+          className="input"
+          type="number"
+          placeholder="RollNo"
+          {...register('RollNo', { required: true })}
+          onInput={handleInputChange} // Convert input to uppercase
+        />
+        <input
+          className="input"
+          type="text"
+          placeholder="Batch"
+          {...register('Batch', { required: true })}
+          onInput={handleInputChange} // Convert input to uppercase
+        />
+        <button className="button" type="submit">Generate ID Card</button>
+      </form>
+      {/* Display the QR code */}
+      {id && (
+        <div className="qr-code-container">
+          <div className="qr-code">
+            <QRCode value={id} size={256} />
+          </div>
+          <button className="download-button" onClick={handleDownload}>
+            Download QR Code
+          </button>
+        </div>
+      )}
     </div>
   );
 }
