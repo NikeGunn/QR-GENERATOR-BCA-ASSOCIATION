1a1df631de5b45a14f197fb52881f29727d19d49
NIkhil <knewboy.nykhil@gmail.com>
2023-08-01 16:25:39 +0545

    done changes on handleDownload function make it more dynamic by adding pdf file functionality

--- src/App.js
+++ src/App.js
@@ -3,10 +3,14 @@ import { useForm } from 'react-hook-form';
 import axios from 'axios';
 import QRCode from 'qrcode.react';
 import './App.css';
+import jsPDF from 'jspdf';
 
 function App() {
   const { register, handleSubmit, reset } = useForm();
   const [id, setID] = useState('');
+  const [name, setName] = useState('');
+  const [rollNo, setRollNo] = useState('');
+  const [batch, setBatch] = useState('');
 
   const onSubmit = async (data) => {
     try {
@@ -15,6 +19,9 @@ function App() {
       });
       console.log(response.data);
       setID(response.data._id);
+      setName(response.data.name);
+      setRollNo(response.data.RollNo);
+      setBatch(response.data.Batch);
       reset();
     } catch (error) {
       console.error(error);
@@ -22,12 +29,61 @@ function App() {
   };
 
   const handleDownload = () => {
+    // Show a custom modal with a friendly message to the user
+    const modal = document.createElement("div");
+    modal.classList.add("modal");
+    modal.innerHTML = `
+      <div class="modal-content">
+        <h2 class="modal-title">Generating ID Card</h2>
+        <p class="modal-message">Please wait for a moment while your ID Card is being created...</p>
+      </div>
+    `;
+    document.body.appendChild(modal);
+
+    // Create a new PDF document
+    const pdfDoc = new jsPDF({
+      orientation: 'portrait',
+      unit: 'mm',
+      format: 'a4',
+      putOnlyUsedFonts: true,
+      compress: true,
+      });
+    const qrCodeSize = 80;
+    const qrCodeX = 65;
+    const qrCodeY = 40;
+
+    // Add ID card background (you can replace 'PUT_YOUR_IMAGE_URL_HERE' with the actual URL of the background image)
+    const imgData = 'PUT_YOUR_IMAGE_URL_HERE';
+    pdfDoc.addImage(imgData, 'JPEG', 0, 0, 210, 297);
+
+    // Add the QR code image to the PDF document
     const canvas = document.querySelector("canvas");
-    const qrCodeURL = canvas.toDataURL("image/png");
-    const downloadLink = document.createElement("a");
-    downloadLink.href = qrCodeURL;
-    downloadLink.download = "mmamcbca_qrcode.png";
-    downloadLink.click();
+    const qrCodeDataURL = canvas.toDataURL("image/png");
+    pdfDoc.addImage(qrCodeDataURL, "PNG", qrCodeX, qrCodeY, qrCodeSize, qrCodeSize);
+
+    // Add student's name to the PDF document
+    pdfDoc.setFont('helvetica', 'bold');
+    pdfDoc.setFontSize(30, 'bold');
+    pdfDoc.setTextColor('purple');
+    pdfDoc.text('BCA ASSOCIATION', 105, 20, { align: 'center', maxWidth: 100 });
+    pdfDoc.setFontSize(20, 'bold');
+    pdfDoc.setTextColor('black');
+    pdfDoc.text(name, 105, 32, { align: 'center', maxWidth: 100 });
+
+    // Add roll number and batch to the PDF document
+    pdfDoc.setFontSize(20, 'bold');
+    pdfDoc.text(`Roll No: ${rollNo}`, 105, 135, { align: 'center', maxWidth: 100 });
+    pdfDoc.setFontSize(35, 'bold');
+    pdfDoc.setTextColor('dodgerblue');
+    pdfDoc.text(`Batch : ${batch}`, 105, 150, { align: 'center', maxWidth: 100,  });
+
+    // Save the PDF with the person's name
+    pdfDoc.save(`bca_association_${name}.pdf`);
+
+    // Remove the modal after a brief delay (2 seconds)
+    setTimeout(() => {
+      document.body.removeChild(modal);
+    }, 1000);
   };
 
   const handleInputChange = (event) => {
@@ -37,8 +93,8 @@ function App() {
   return (
     <div className="container">
       <nav className="navbar">
-        <h1 className="navbar-title">MMAMC BCA ASSOCIATION </h1>
-        <h2 className="navbar-subtitle">ID CARD GENERATOR </h2>
+        <h1 className="navbar-title">MMAMC BCA ASSOCIATION</h1>
+        <h2 className="navbar-subtitle">ID CARD GENERATOR</h2>
       </nav>
       <form className="form" onSubmit={handleSubmit(onSubmit)}>
         <input
